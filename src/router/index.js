c76176c39eccb303f62f1a68afc59fcf00e09e53
LuoDan <1178886976@qq.com>
2018-07-08 16:05:04 +0800

    用户列表子路由打通

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,8 @@ import Vue from 'vue';
 import Router from 'vue-router';
 import Login from '@/views/Login';
 import Home from '@/views/Home';
+import Users from '@/views/users/Users';
+
 
 Vue.use(Router);
 
@@ -15,7 +17,17 @@ export default new Router({
     {
       name: 'home',
       path: '/',
-      component: Home
+      component: Home,
+      // 子路由 子路由切换时在home组件中完成的
+      // 使用子路由之前 home组件已经加载完毕
+      children:[
+        // 用户列表
+        {
+          name: 'users',
+          path: '/users',
+          component: Users
+        }
+      ]
     }
   ]
 });

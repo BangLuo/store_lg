b7c508e2228506d3354d3c59d5448cf1e17f7d72
LuoDan <1178886976@qq.com>
2018-07-08 16:11:10 +0800

    card卡片与面包屑 布局

--- src/views/users/Users.vue
+++ src/views/users/Users.vue
@@ -1,6 +1,13 @@
 <template>
     <div>
-        users
+      <el-card class="box-card">
+        <!-- 面包屑 -->
+        <el-breadcrumb separator-class="el-icon-arrow-right">
+          <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
+          <el-breadcrumb-item>用户管理</el-breadcrumb-item>
+          <el-breadcrumb-item>用户列表</el-breadcrumb-item>
+        </el-breadcrumb>
+      </el-card>
     </div>
 </template>
 

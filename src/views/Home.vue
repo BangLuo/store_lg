163e07af6e974141eb643566e0af0770a1355525
LuoDan <1178886976@qq.com>
2018-07-08 20:31:44 +0800

    侧边栏布局 补充

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -29,33 +29,43 @@
             </el-submenu>
             <el-submenu index="2">
               <template slot="title"><i class="el-icon-location"></i>权限管理</template>
-              <el-menu-item-group>
-                 <template slot="title"><i class="el-icon-location"></i>权限列表</template>
-                <el-menu-item index="2-1">选项1</el-menu-item>
-                <el-menu-item index="2-2">选项2</el-menu-item>
-              </el-menu-item-group>
-              <el-menu-item-group title="分组2">
-                <el-menu-item index="2-3">选项3</el-menu-item>
-              </el-menu-item-group>
-              <el-submenu index="2-4">
-                <template slot="title">选项4</template>
-                <el-menu-item index="2-4-1">选项4-1</el-menu-item>
-              </el-submenu>
+                 <el-menu-item index="/users">
+                <i class="el-icon-menu"></i>
+                  角色列表
+                </el-menu-item>
+                <el-menu-item index="/users">
+                <i class="el-icon-menu"></i>
+                  权限列表
+                </el-menu-item>
             </el-submenu>
             <el-submenu index="3">
               <template slot="title"><i class="el-icon-location"></i>商品管理</template>
-              <el-menu-item-group>
-                <template slot="title">分组一</template>
-                <el-menu-item index="3-1">选项1</el-menu-item>
-                <el-menu-item index="3-2">选项2</el-menu-item>
-              </el-menu-item-group>
-              <el-menu-item-group title="分组2">
-                <el-menu-item index="3-3">选项3</el-menu-item>
-              </el-menu-item-group>
-              <el-submenu index="3-4">
-                <template slot="title">选项4</template>
-                <el-menu-item index="3-4-1">选项4-1</el-menu-item>
-              </el-submenu>
+                <el-menu-item index="/users">
+                <i class="el-icon-menu"></i>
+                  商品列表
+                </el-menu-item>
+                 <el-menu-item index="/users">
+                <i class="el-icon-menu"></i>
+                  分类参数
+                </el-menu-item>
+                 <el-menu-item index="/users">
+                <i class="el-icon-menu"></i>
+                  商品分类
+                </el-menu-item>
+            </el-submenu>
+                <el-submenu index="4">
+              <template slot="title"><i class="el-icon-location"></i>订单管理</template>
+                <el-menu-item index="/users">
+                <i class="el-icon-menu"></i>
+                  订单列表
+                </el-menu-item>
+            </el-submenu>
+            <el-submenu index="5">
+              <template slot="title"><i class="el-icon-location"></i>数据统计</template>
+                <el-menu-item index="/users">
+                <i class="el-icon-menu"></i>
+                  数据列表
+                </el-menu-item>
             </el-submenu>
           </el-menu>
         </el-aside>

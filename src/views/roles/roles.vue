c14eb16d858f62d5fd0c7bc497c0a19c7a91509c
LuoDan <1178886976@qq.com>
2018-07-11 09:51:29 +0800

    角色列表 布局完成

--- src/views/roles/roles.vue
+++ src/views/roles/roles.vue
@@ -1,15 +1,65 @@
 <template>
     <div>
-        ajsvidc
+        <el-card class="box-card">
+            <!-- 面包屑 -->
+          <el-breadcrumb separator-class="el-icon-arrow-right">
+          <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
+          <el-breadcrumb-item>权限管理</el-breadcrumb-item>
+          <el-breadcrumb-item>角色列表</el-breadcrumb-item>
+          </el-breadcrumb>
+          <!-- 添加角色 -->
+          <el-row class="row-add">
+              <el-col :span="24">
+                  <el-button>
+                      添加角色
+                  </el-button>
+              </el-col>
+          </el-row>
+          <!-- 表格 -->
+          <el-table
+            height="500"
+            class="tableRoles"
+            stripe
+            border
+            :data="list"
+            style="width: 100%">
+            <el-table-column
+            type="index"
+            width="50">
+            </el-table-column>
+            <el-table-column
+              prop="authName"
+              label="角色名称"
+              width="150">
+            </el-table-column>
+            <el-table-column
+              prop="path"
+              label="角色描述"
+              width="150">
+            </el-table-column>
+            <el-table-column
+              prop="level"
+              label="操作">
+            </el-table-column>
+          </el-table>
+        </el-card>
+
     </div>
 </template>
 
 <script>
 export default {
-
+    data () {
+    return {
+      list: []
+    };
+  }
 };
 </script>
 
 <style>
-
+.row-add {
+    margin-top: 15px;
+    margin-bottom: 15px;
+}
 </style>
